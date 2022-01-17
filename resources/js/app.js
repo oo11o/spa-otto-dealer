a0cca546b04157651612893b84d06f20fb21477f
Vlad Imir <ymyr.vlad@gmail.com>
2022-01-17 17:55:16 +0200

    add Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 require('./bootstrap');
+
+import { createApp } from 'vue';
+
+import App from './components/Index'
+
+
+const app = createApp({
+    components: {
+       App
+    }
+})
+
+app.mount('#app')
---
+++ resources/js/components/Index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>000000000000</h1>
+    </div>
+</template>
+
+<script>
+    
+</script>
