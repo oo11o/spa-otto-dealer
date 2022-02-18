b820f513e855ed6931f3a2c9b6c7706e49be0fab
Vlad Imir <ymyr.vlad@gmail.com>
2022-02-18 16:44:54 +0200

    fix eloquent response

--- app/Http/Controllers/Api/UserValidateController.php
+++ app/Http/Controllers/Api/UserValidateController.php
@@ -13,8 +13,9 @@ class UserValidateController extends Controller
     public function index(Request $request){
 
 
-        $result = TetraUsers::where('email', $request->login)
+        $result = TetraUsers::select('user.*','tetra_customer.saldo')->where('email', $request->login)
             ->where('pass_md5', md5($request->pass))
+            ->leftJoin('tetra_customer', 'user.id_tetra' , '=', 'tetra_customer.id_tetra')
             ->get()
             ->toArray();
 
